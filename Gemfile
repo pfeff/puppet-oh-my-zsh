9bd44c3250ad1b627590dbaad66e067707f6aae0
Matt Pfefferle <matthew.pfefferle@gmail.com>
2013-04-04 16:36:19 -0400

    Init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "cardboard", "~> 1.0"
+gem "librarian-puppet"
+gem "puppet-lint"
+
---
+++ manifests/init.pp
@@ -0,0 +1,5 @@
+class oh-my-zsh {
+  $install = "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
+  exec { "$install": }
+}
+
