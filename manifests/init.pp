cfe6edb91fc53f0c2e83732b9436a6ce288c413d
Matt Pfefferle <matthew.pfefferle@gmail.com>
2013-04-05 09:50:00 -0400

    Added 'creates' so exec only runs once

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 class oh-my-zsh {
   $install = "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
-  exec { "$install": }
+  exec { "$install":
+    creates => "/Users/${::luser}/.oh-my-zsh"
+  }
 }
 
